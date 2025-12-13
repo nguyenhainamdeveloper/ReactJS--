d50c0fbfbe1538b264a39c6e7263d0fa3670eedb
Nguyen Hai Nam <namkaizin@gmail.com>
2025-12-14 00:01:09 +0700

    Video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div>Nguyen Hai Nam My Component</div>
+        <>
+            <div>Nguyen Hai Nam My Component</div>
+            <div className="child" style={{ borderRadius: "5px" }}>Anh zin</div>
+        </>
     );
 }
 
