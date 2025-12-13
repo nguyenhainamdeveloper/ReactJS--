5f00e54bb42e641a47ffa2238bd204cc7b4e94df
Nguyen Hai Nam <namkaizin@gmail.com>
2025-12-14 00:14:14 +0700

    Video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const nguyenhainam = "kaizin";
+    // const nguyenhainam = 25;
+    // const nguyenhainam = true;
+    // const nguyenhainam = [1, 2, 3, 4]
+    const nguyenhainam = {
+        name: "nguyenhainam",
+        nickname: "kaizin"
+    }
     return (
         <>
-            <div>Nguyen Hai Nam My Component</div>
+            <div>Nguyen Hai Nam & {JSON.stringify(nguyenhainam)} </div>
             <div className="child" style={{ borderRadius: "5px" }}>Anh zin</div>
         </>
     );
