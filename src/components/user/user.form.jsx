560e100af38c45a354a9aa9c88b20cbf0ecd3532
Nguyen Hai Nam <namkaizin@gmail.com>
2026-03-01 22:25:00 +0700

    video 67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,28 +1,59 @@
 import { Input, Button } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+
+    const [fullName, setFullName] = useState("hoidanit");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleClickBtn = () => {
+        console.log(">>>> check state: ", { fullName, email, password, phone })
+    }
+
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
+
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                 </div>
+
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
+
                 <div>
                     <span>Phone Number</span>
-                    <Input />
+                    <Input
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                    />
                 </div>
+
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button
+                        onClick={() => handleClickBtn()}
+                        type="primary">Create User</Button>
                 </div>
+
             </div>
         </div>
     );
